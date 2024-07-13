29cf6a0091e4e900ca7ac0635ed63aecbf86980c
alexnickolaev <alexnik22@gmail.com>
2024-07-13 17:35:23 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public String surname;
+    public static String newValue = "";
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c) {
+
+    }
+
+    String name;
+
+    Broken() {
+    }
+
+}
